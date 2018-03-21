97a400195356d4c48f5c0c29aec0a4ca6f755758
Alvin.Qu <quhao@wisdomgarden.com>
2018-03-21 14:15:05 +0800

    create project

---
+++ pearls/src/sort.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "sort.h"
+
+void show() {
+    printf("Hello\n");
+}
---
+++ pearls/src/sort.h
@@ -0,0 +1,10 @@
+//
+// Created by Alvin.Qu on 20/03/2018.
+//
+
+#ifndef STUDY_CPP_SORT_H
+#define STUDY_CPP_SORT_H
+
+void show();
+
+#endif //STUDY_CPP_SORT_H

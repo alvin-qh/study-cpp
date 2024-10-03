eafb587c7bfb5726fec95fe87853c87f80957b5b
AlvinQ <hqu@hipacloud.com>
2024-10-03 23:17:00 +0800

    [cplusplus] add sfinae demo

--- cplusplus/include/template/sfinae.hpp
+++ cplusplus/include/template/sfinae.hpp
@@ -10,17 +10,45 @@
 #define __CPLUSPLUS_TEMPLATE_SFINAE_H
 
 namespace cpp {
+	/// @brief 定义类型, 内部包含 `type_x` 类型定义
+	///
+	/// `type_x` 类型为 `int` 类型的别名
 	struct X {
-		typedef int type1;
+		typedef int type_x;
 	};
 
+	/// @brief 定义类型, 内部包含 `type_y` 类型定义
+	///
+	/// `type_y` 类型为 `int` 类型的别名
 	struct Y {
-		typedef int type2;
+		typedef int type_y;
 	};
 
-	template <typename T> void foo(typename T::type1) {};    // Foo0
-	template <typename T> void foo(typename T::type2) {};    // Foo1
-	template <typename T> void foo(T) {};                    // Foo2
+	/// @brief 当 `T` 类型内部具备 `T::type_x` 类型时, `foo` 模板函数的特化
+	///
+	/// @tparam T 具备 `T::type_x` 类型定义的类型
+	template <typename T> const char* foo(typename T::type_x) { return "foo-type-x"; }
+
+	/// @brief 当 `T` 类型内部具备 `T::type_y` 类型时, `foo` 模板函数的特化
+	///
+	/// @tparam T 具备 `T::type_y` 类型定义的类型
+	template <typename T> const char* foo(typename T::type_y) { return "foo-type-y"; }
+
+	/// @brief 当 `foo` 函数的所有特化都不满足时, 编译器选择 `foo` 函数
+	template <typename T> const char* foo(T) { return "foo-type-any"; }
+
+	struct true_type {
+		static constexpr bool value = true;
+	};
+
+	struct false_type {
+		static constexpr bool value = false;
+	};
+
+	template<T>
+	struct enable_if {
+		typedef true_type type;
+	};
 }
 
 #endif // ! __CPLUSPLUS_TEMPLATE_SFINAE_H
--- cplusplus/test/template/test_sfinae.cpp
+++ cplusplus/test/template/test_sfinae.cpp
@@ -7,6 +7,22 @@
 
 using namespace cpp;
 
-TEST(TEST_SUITE_NAME, sfinae) {
-	
+/// @brief 测试模板函数的 SFINAE
+TEST(TEST_SUITE_NAME, function_sfinae) {
+	const char* r = nullptr;
+
+	// `X::type_x` 存在, 故将匹配到 `foo(typename T::type_x)` 模板函数
+	// 其它 `foo` 模板函数匹配失败
+	r = foo<X>(1);
+	ASSERT_EQ(r, "foo-type-x");
+
+	// `X::type_y` 存在, 故将匹配到 `foo(typename T::type_y)` 模板函数
+	// 其它 `foo` 模板函数匹配失败
+	r = foo<Y>(1);
+	ASSERT_EQ(r, "foo-type-y");
+
+	// `foo(typename T::type_x)` 与 `foo(typename T::type_y)`
+	// 模板均匹配失败, 故 `foo(T)` 模板函数匹配成功
+	r = foo<int>(1);
+	ASSERT_EQ(r, "foo-type-any");
 }

f36855527dbd5ac6b75567a531ad5a4328c17e2b
AlvinQ <hqu@hipacloud.com>
2024-10-05 00:13:48 +0800

    [cplusplus] add reference demo

--- cplusplus/include/reference/ref.hpp
+++ cplusplus/include/reference/ref.hpp
@@ -4,6 +4,17 @@
 #define __CPLUSPLUS_REFERENCE_REF_H
 
 namespace cpp {
+	/// @brief 交换两个变量值
+	///
+	/// @tparam T 被交换值的类型
+	/// @param a 被交换的两个值
+	/// @param b 被交换的两个值
+	template<typename T>
+	inline void swap(T& a, T& b) {
+		T tmp = a;
+		a = b;
+		b = tmp;
+	}
 } // ! namespace cpp
 
 #endif // ! __CPLUSPLUS_REFERENCE_REF_H
--- cplusplus/test/reference/test_ref.cpp
+++ cplusplus/test/reference/test_ref.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include "reference/ref.hpp"
 
 #include <gtest/gtest.h>
@@ -6,3 +8,16 @@
 
 using namespace std;
 using namespace cpp;
+
+/// @brief 测试引用类型函数参数
+TEST(TEST_SUITE_NAME, ref_arguments) {
+	int a = 100, b = 200;
+	cpp::swap(a, b);
+	ASSERT_EQ(a, 200);
+	ASSERT_EQ(b, 100);
+
+	string s1 = "Hello", s2 = "World";
+	cpp::swap(s1, s2);
+	ASSERT_EQ(s1, "World");
+	ASSERT_EQ(s2, "Hello");
+}

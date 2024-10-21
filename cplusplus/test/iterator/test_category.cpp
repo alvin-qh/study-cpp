fadb1e3909edfc4c592e18d3121273cef3dda984
AlvinQ <hqu@hipacloud.com>
2024-10-22 02:14:53 +0800

    [cplusplus] update iterator category demo

--- cplusplus/test/iterator/test_category.cpp
+++ cplusplus/test/iterator/test_category.cpp
@@ -1,3 +1,10 @@
 #include <gtest/gtest.h>
 
 #define TEST_SUITE_NAME test_cplusplus_iterator_category
+
+
+/// `input_iterator_tag`
+/// `output_iterator_tag`
+/// `forward_iterator_tag`
+/// `bidirectional_iterator_tag`
+/// `random_access_iterator_tag`

c182b0978ca047053cc6ecc3207b8edf6dda9fc3
Alvin <quhao317@163.com>
2024-11-25 00:11:25 +0800

    [cplusplus] add CRTP demo

---
+++ cplusplus/include/template/crtp.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef __CPLUSPLUS_TEMPLATE__CRTP_H
+#define __CPLUSPLUS_TEMPLATE__CRTP_H
+
+namespace cxx::templated {
+
+} // namespace cxx::templated
+
+#endif //__CPLUSPLUS_TEMPLATE__CRTP_H
---
+++ cplusplus/test/template/test_crtp.cc
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+#include "template/crtp.h"
+
+#define TEST_SUITE_NAME test_cplusplus_template__CRTP
+
+using namespace cxx::templated;
+

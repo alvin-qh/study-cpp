f6556552c5a4a2077b908b0ce29417154c57553e
Alvin <quhao317@163.com>
2024-11-25 00:15:11 +0800

    [cplusplus] update CRTP demo

--- cplusplus/include/template/crtp.h
+++ cplusplus/include/template/crtp.h
@@ -3,6 +3,13 @@
 #ifndef __CPLUSPLUS_TEMPLATE__CRTP_H
 #define __CPLUSPLUS_TEMPLATE__CRTP_H
 
+/// CRTP 是 C++ 模板的一种使用范式, 称为 "奇异递归模板模式 (Curiously Recurring Template Pattern)"
+///
+/// 在一些情况下, CRTP 可以替代 C++ OOP 中的基础和多态 (虚函数), 并且不会引入虚表 (VTable)
+/// 从而导致性能下降和内存使用升高
+///
+/// https://zhuanlan.zhihu.com/p/142407249
+
 namespace cxx::templated {
 
 } // namespace cxx::templated

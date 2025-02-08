e6cc77355a45caa0c3f7d984cf9147643ee7fb4e
Alvin <quhao317@163.com>
2025-02-09 00:40:55 +0800

    [linux] add pthread demo

--- cplusplus/test/test.h
+++ cplusplus/test/test.h
@@ -8,6 +8,7 @@
 namespace cxx {
 
 	/// @brief 对浮点数进行指定小数位数保留
+	///
 	/// @param n 浮点数数值
 	/// @param fixed 要保留的小数位数
 	/// @return 保留小数后的结果
--- cplusplus/test/test_main.cc
+++ cplusplus/test/test_main.cc
@@ -15,11 +15,20 @@ int main(int argc, char* argv[]) {
 
 namespace cxx {
 
+    /// @brief 保留参数 `n` 小数点后的 `fixed` 位
+    ///
+    /// @param n 要保留小数位的数值
+    /// @param fixed 要保留的小数位
+    /// @return 计算结果
     double number_to_fixed(double n, uint32_t fixed) {
+        // 计算 `10` 的 `fixed` 次方, 得到一个 `1` 后面 `fixed` 个 `0` 的整数
         uint32_t p = pow(10, fixed);
         if (n > 0) {
+            // 将 `n` 的小数点向右移动 `p` 位后舍弃小数部分, 再除以 `p`, 得到小数点后 `p` 位的计算结果
             return round(n * p) / p;
         }
+
+        // 处理 `n` 为负数的情况
         return -round(-n * p) / p;
     }
 
